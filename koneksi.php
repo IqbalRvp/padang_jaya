2f084413453107f138f98a432a2e2b49f15a2b41
IqbalRvp <iqballrvp1912@gmail.com>
2024-05-27 21:57:21 +0700

    fix koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","toskin");
+$koneksi = mysqli_connect("103.210.69.63","root","root","toskin");
 if (mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
 }
